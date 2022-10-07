fdb0bc39659c84e70004de56cee0c0d51014e7a8
Paul Barker <paul.barker@sancloud.com>
2022-10-07 10:27:53 +0100

    Allow log statements to be compiled out
    Signed-off-by: Paul Barker <paul.barker@sancloud.com>

--- src/femtolog.h
+++ src/femtolog.h
@@ -29,12 +29,50 @@ void femtolog_vlog(int level, const char *fmt, va_list args);
 void femtolog_log(int level, const char *fmt, ...) __attribute__((format(printf, 2, 3)));
 
 #ifndef FEMTOLOG_NO_MACROS
+#ifndef FEMTOLOG_MIN_LEVEL
+#define FEMTOLOG_MIN_LEVEL 0
+#endif
+
+#if (FEMTOLOG_MIN_LEVEL < FEMTOLOG_TRACE) || (FEMTOLOG_MIN_LEVEL > FEMTOLOG_FATAL)
+#error FEMTOLOG_MIN_LEVEL is invalid
+#endif
+
+#if FEMTOLOG_MIN_LEVEL <= FEMTOLOG_TRACE
 #define log_trace(...) femtolog_log(FEMTOLOG_TRACE, __VA_ARGS__)
+#else
+#define log_trace(...)
+#endif
+
+#if FEMTOLOG_MIN_LEVEL <= FEMTOLOG_DEBUG
 #define log_debug(...) femtolog_log(FEMTOLOG_DEBUG, __VA_ARGS__)
+#else
+#define log_debug(...)
+#endif
+
+#if FEMTOLOG_MIN_LEVEL <= FEMTOLOG_INFO
 #define log_info(...) femtolog_log(FEMTOLOG_INFO, __VA_ARGS__)
+#else
+#define log_info(...)
+#endif
+
+#if FEMTOLOG_MIN_LEVEL <= FEMTOLOG_WARN
 #define log_warn(...) femtolog_log(FEMTOLOG_WARN, __VA_ARGS__)
+#else
+#define log_warn(...)
+#endif
+
+#if FEMTOLOG_MIN_LEVEL <= FEMTOLOG_ERROR
 #define log_error(...) femtolog_log(FEMTOLOG_ERROR, __VA_ARGS__)
+#else
+#define log_error(...)
+#endif
+
+#if FEMTOLOG_MIN_LEVEL <= FEMTOLOG_FATAL
 #define log_fatal(...) femtolog_log(FEMTOLOG_FATAL, __VA_ARGS__)
+#else
+#define log_fatal(...)
+#endif
+
 #define log_dynamic(level, ...) femtolog_log(level, __VA_ARGS__)
 #endif
 
@@ -50,6 +88,10 @@ void femtolog_log(int level, const char *fmt, ...) __attribute__((format(printf,
 #endif
 
 #ifndef FEMTOLOG_NO_MACROS
+#if FEMTOLOG_MIN_LEVEL <= FEMTOLOG_FATAL
 #define log_fatal_exit(...) do { femtolog_log(FEMTOLOG_FATAL, __VA_ARGS__); exit(1); } while (0)
+#else
+#define log_fatal_exit(...) exit(1)
 #endif
+#endif /* FEMTOLOG_NO_MACROS */
 #endif /* FEMTOLOG_FREESTANDING */
